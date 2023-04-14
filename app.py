8e911c8bc2b728dda941ae21f4dd04f963182fa9
KSKrishna1410 <115217350+KSKrishna1410@users.noreply.github.com>
2023-04-14 18:02:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import pandas as pd
 from preprocess import preprocess_data
 import base64
 from PIL import Image
-import openpyxl
+
 
 # Set the app title and logo
-st.title("INSOFE Faculty Report Application")
+st.title("INSOFE-Faculty Report Application")
 
 # File uploader widget
 file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 
 if file is not None:
     file_extension = file.name.split(".")[-1]
-    file_name = file.name
+    
     # Load the dataset into a Pandas DataFrame
     if file_extension == "csv":
         df = pd.read_csv(file)
@@ -25,8 +25,10 @@ if file is not None:
     
     # Display the processed dataset in the app
     st.write(df_processed)
+    
     # Add a button to download the processed dataset as Excel
-    excel_file = df_processed.to_excel("final.xlsx",index=True, engine='openpyxl')
-    b64 = base64.b64encode(excel_file).decode()
+    excel = df_processed.to_excel("processed_data.xlsx", index=True, engine='openpyxl')
+   
+    b64 = base64.b64encode(excel).decode()
     href = f'<a href="data:application/octet-stream;base64,{b64}" download="processed_data.xlsx">Download processed data</a>'
     st.markdown(href, unsafe_allow_html=True)
